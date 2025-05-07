e856fd86373d45f85e5a38d4cb4f476596b76220
Abhi-CloudDevops <abhishekpandey8123@gmail.com>
2025-05-07 13:32:44 +0530

    Update main.tf

--- day-10-UserData/main.tf
+++ day-10-UserData/main.tf
@@ -1,5 +1,8 @@
+provider "aws" {
+  region = "ap-south-1"
+}
 resource "aws_instance" "my_instance" {
-  ami                    = "ami-0f1dcc636b69a6438" 
+  ami                    = "ami-062f0cc54dbfd8ef1" 
   instance_type          = "t2.micro"              
   associate_public_ip_address = true                
 #   key_name               = "my-key-pair"           
