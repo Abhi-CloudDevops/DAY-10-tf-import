43f2c51779bfd6f42f8b67d8e12d3a7252e81ee8
Abhi-CloudDevops <abhishekpandey8123@gmail.com>
2025-05-07 13:36:51 +0530

    Update main.tf

--- day-10-UserData/main.tf
+++ day-10-UserData/main.tf
@@ -1,6 +1,7 @@
 provider "aws" {
   region = "ap-south-1"
 }
+
 resource "aws_instance" "my_instance" {
   ami                    = "ami-062f0cc54dbfd8ef1" 
   instance_type          = "t2.micro"              
