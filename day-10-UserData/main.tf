231e22b0fb477cafe8388e8ca6b48add6332416d
Abhishek <abhishekpandey8123@gmail.com>
2025-11-20 10:57:05 +0530

    Update main.tf

--- day-10-UserData/main.tf
+++ day-10-UserData/main.tf
@@ -10,7 +10,7 @@ resource "aws_instance" "my_instance" {
     #!/bin/bash
     yum update -y
     yum install -y httpd
-    echo "Hello, World!" > /var/www/html/index.html
+    echo "Hello from Jenkins Deployment!" > /var/www/html/index.html
     systemctl start httpd
     systemctl enable httpd
   EOF
